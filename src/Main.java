ad2a9e665dde164ba34c38062c4d84d8e40af44a
Abhinav Garg <maintoabhinavgarg@gmail.com>
2024-09-11 06:26:43 +0530

    uncommited changes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Welcome to the Game of Life! ⊂(◉‿◉)つ");
+        System.out.println("Please input coordinates in integer of live cells in separate line in format `x, y`");
+        System.out.println("Any input not in this form would be ignored");
+        System.out.println("When you're finished, please enter END");
+        System.out.println("Have Fun! (ง •̀◡•́)ง");
+
         List<Cell> liveCells = getLiveCellsByUserInput();
 
         LifeBoard lifeBoard = new LifeBoard(liveCells);
@@ -13,12 +19,6 @@ public class Main {
     }
 
     private static List<Cell> getLiveCellsByUserInput() {
-        System.out.println("Welcome to the Game of Life! ⊂(◉‿◉)つ");
-        System.out.println("Please input coordinates in integer of live cells in separate line in format `x, y`");
-        System.out.println("Any input not in this form would be ignored");
-        System.out.println("When you're finished, please enter END");
-        System.out.println("Have Fun! (ง •̀◡•́)ง");
-
         List<Cell> liveCells = new ArrayList<>();
         Scanner userInput = new Scanner(System.in);
         String enteredValue = userInput.nextLine();
